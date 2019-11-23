9aee7027169774ea1cacbd9daf8e66a350037ff7
Orxan-dev <54859397+Orxan-dev@users.noreply.github.com>
2019-11-23 18:22:50 +0400

    Add files via upload

---
+++ final lahiye19.11.2019/js/main.js
@@ -0,0 +1,77 @@
+
+$(window).scroll(function(){
+  if($(window).scrollTop()>250){
+      $(".navbar").addClass("colorzilla")
+  }
+  else( 
+      $(".navbar").removeClass("colorzilla"))
+}),
+
+
+
+$(".slider").slick({
+    slidesToShow:4,
+    infinity:true,
+    autoplay:400,
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+})
+$(".img-blog2").slick({
+  slidesToShow:1,
+  infinity:true,
+  autoplay:200,
+  responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+})
